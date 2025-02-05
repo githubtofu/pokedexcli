9eb998d393470bece51dd3eed2deea8dccede035
obviouswinter30 <slowdrop@gmail.com>
2025-02-05 12:23:40 +0900

    pokeapi map, mapb

--- main.go
+++ main.go
@@ -5,13 +5,21 @@ import (
     "bufio"
     "os"
     "github.com/mtslzr/pokeapi-go"
-//    "reflect"
+    "github.com/mtslzr/pokeapi-go/structs"
+    "io"
+    "encoding/json"
+    "net/http"
 )
 
 type cliCommand struct {
     name    string
     description string
-    callback func() error
+    callback func(*config) error
+}
+
+type config struct {
+    Next string
+    Previous string
 }
 
 var climap = map[string]cliCommand{}
@@ -22,20 +30,85 @@ func cleanInput(text string) []string {
     return words
 }	
 
-func commandMap() error {
-    l, err := pokeapi.Resource("location-area")
-//    fmt.Printf("First Type:%v, Second Type:%v\n", reflect.TypeOf(l), reflect.TypeOf(l1))
-    fmt.Println(l)
-    return err
+func getResults(url string, c *config) ([]structs.Result, error) {
+    results := []structs.Result{}
+    res, err := http.Get(url)
+    if err != nil {
+        return results, err
+    }
+    defer res.Body.Close()
+    body, err := io.ReadAll(res.Body)
+    if res.StatusCode > 299 {
+        return results, fmt.Errorf("Response failed with code : %d and \nbody: %s\n", res.StatusCode, body)
+    }
+    if err != nil {
+        return results, err
+    }
+    resource := structs.Resource{}
+    err = json.Unmarshal(body, &resource)
+    if err != nil {
+        return results, err
+    }
+    results = resource.Results
+    c.Next = resource.Next
+    if resource.Previous != nil {
+        c.Previous = resource.Previous.(string)
+    } else {
+        c.Previous = ""
+    }
+    return results, nil
 }
 
-func commandExit() error {
+func commandMap(c *config) error {
+    results := []structs.Result{}
+    if c.Next == "" {
+        l, err := pokeapi.Resource("location-area")
+        if err != nil {
+            return err
+        }
+        c.Next = l.Next
+        if l.Previous != nil {
+            c.Previous = string(l.Previous.(string))
+        }
+        results = l.Results
+    } else {
+        r, err := getResults(c.Next, c)
+        if err != nil {
+            return err
+        }
+        results = r
+    }
+    for _, a_result := range results {
+        fmt.Println(a_result.Name)
+    }
+    return nil
+}
+
+func commandMapb(c *config) error {
+    results := []structs.Result{}
+    if c.Previous == "" {
+        fmt.Println("you're on the first page")
+        return nil
+    } else {
+        r, err := getResults(c.Previous, c)
+        if err != nil {
+            return err
+        }
+        results = r
+    }
+    for _, a_result := range results {
+        fmt.Println(a_result.Name)
+    }
+    return nil
+}
+
+func commandExit(c *config) error {
     fmt.Printf("Closing the Pokedex... Goodbye!\n")
     os.Exit(0)
     return nil
 }
 
-func commandHelp() error {
+func commandHelp(c *config) error {
     fmt.Println("Welcome to the Pokedex!")
     fmt.Println("Usage:")
     fmt.Println("")
@@ -61,10 +134,16 @@ func set_map() {
             description: "Displays the names of 20 location areas in the Pokemon world",
             callback: commandMap,
         }
+    climap["mapb"] = cliCommand{
+            name:   "mapb",
+            description: "Displays the names of previous 20 location areas in the Pokemon world",
+            callback: commandMapb,
+        }
 }
 
 func main() {
     set_map()
+    this_conf := config{}
     scanner := bufio.NewScanner(os.Stdin)
     for {
         fmt.Print("Pokedex >")
@@ -77,7 +156,7 @@ func main() {
             }
             for k, v := range climap {
                 if k == tokens[0] {
-                    err := v.callback()
+                    err := v.callback(&this_conf)
                     if err != nil {
                         fmt.Println(err)
                     }
